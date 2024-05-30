9b21270f32793fa793a388bb24879f2bc73a0d03
rafacostta <rafael.costta13@gmail.com>
2024-05-30 02:03:12 -0300

    Variaveis

---
+++ 01 - variaveis.rb
@@ -0,0 +1,18 @@
+# CONSTANTES
+# Nome de variável em maiúsculo
+
+NOME_VARIAVEL = "Γειά σου Κόσμε"
+puts NOME_VARIAVEL
+
+
+# VARIAVEIS
+# Variáveis em Ruby tem tipos dinâmicos de dados.
+
+nome = "Rafael"
+idade = 10
+you_are = true
+puts "#{nome.class} | #{idade.class} | #{you_are.class}"
+
+## Atribuição paralela
+a, b ,c = "A", "B", 'C'
+puts "#{a} #{b} #{c}"
