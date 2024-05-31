98f37625a27e0c14dfcddd1ba80ea32ed2926742
rafacostta <rafael.costta13@gmail.com>
2024-05-31 13:08:07 -0300

    Escopo de variaveis

---
+++ 02 - escopo de variaveis.rb
@@ -0,0 +1,24 @@
+# ESCOPO DE VARIÁVEIS
+=begin
+    Escopo define onde uma variável está acessível.
+    Podendo ser uma variável: local, global, instância e classe.
+
+    Cada tipo de variável é declarada usando um caractere especial na frente da variável.
+
+    $   -   Variável global
+    @   -   Variável de instância
+    [a-z] ou _  -   Variável local
+    [A-Z]   -   Constante
+    @@  -   Variável de classe
+=end
+
+=begin
+    MÉTODOS ÚTEIS
+
+    DESCOBRIR ESCOPO DE VARIAVEL
+    defined? nome_variavel
+=end
+
+
+
+
