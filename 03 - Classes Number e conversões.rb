681e554ce23911718993ca09d3717bc3c8be06d4
rafacostta <rafael.costta13@gmail.com>
2024-05-31 21:32:48 -0300

    Classes Number

---
+++ 03 - Classes Number e conversões.rb
@@ -0,0 +1,40 @@
+# CLASSES NUMBER E CONVERSÕES
+
+# CLASSE NUMBER
+    # Classe base para números
+
+# CLASSE FIXNUM
+    # Basicamente são valores inteiros, seu intervalo de valor depende do sistema operacional.
+
+# CLASSE BIGNUM
+    # São números inteiros que estão fora do intervalo da classe Fixnum.
+
+# CLASSE FLOAT
+    # São npumeros com ponto flutuante, números reais.
+
+# CLASSE RATIONAL
+    # Número que pode ser expresso por uma fração.
+
+
+# ---- CONVERSÃO EM RUBY ----
+
+# Float -> Integer
+puts Integer (3.1415) # 3
+
+# String -> Integer
+puts Integer ("31415") # 31415
+
+# Hexadecimal -> Integer
+puts (0xA4F5D) #675677
+
+# Octal -> Integer
+puts Integer (01231) # 655
+
+# Binário -> Integer
+puts Integer(0101) #65
+
+# Caracter para ASCII caracter code
+puts "F".getbyte(0)
+
+
+# Float - para converter para ponto flutuante
