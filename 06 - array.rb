0cbcac8492d890284dbe85e1ad08737863be1d25
rafacostta <rafael.costta13@gmail.com>
2024-06-03 13:20:31 -0300

    Array

---
+++ 06 - array.rb
@@ -0,0 +1,31 @@
+# ARRAY
+
+
+# Criando Array
+
+# Metodo 'new'
+meses = Array.new
+
+# meses = Array.new(Quantidade_de_elementos)
+# meses =  Array.new(12)
+
+# Usando apenas Array[]
+semanas = Array["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]
+
+# Acessando elementos do array
+
+## Pela posição
+## meses[indice]
+## meses[indice_inial, indice_final] - retorna um array na faixa de valor.
+## meses[1..2] - Acessando uma faixa de valor usando range
+
+## Metodos úteis
+
+# empety? - Retorna se array está vazio
+# size - Retona o tamanho do Array
+# class - Retona o tipo de dado de uma determinada pocisão do array
+# at() - Acessa o dado de uma determinada posição do array.
+# first - Acessa o primeiro elemento
+# last - acessa o último elemento.
+# index(valor) - retorna o valor do indice pelo valor
+# slice(range) - retorna um array na faixa de valor.
