a8eb79df9816b66a275ed54f78c98f7926ba7905
rafacostta <rafael.costta13@gmail.com>
2024-06-04 12:36:10 -0300

    Controle de fluxo

---
+++ 10 - controle de fluxo.rb
@@ -0,0 +1,29 @@
+# CONTROLE DE FLUXO
+x = 5
+y = 10
+
+# if - Executa bloco de código se a condição for VERDADEIRA
+if y == 10
+    puts "Olá IF"
+end
+
+# Outra forma de usar if, eliminando a instrução end
+puts "Olá sou o if" if(y == 10)
+
+# if com operadores logicos
+if(y == 10 && x == 5)
+    puts "If com operador lógico"
+end
+
+# else e elsif
+if x > 5 # Executa se a condição for verdadeira
+    puts "Ola sou o if"
+elsif x < 5 # Executa se a condição for verdadeira 
+    puts "Sou elsif"
+else # executa se nenhuma dos ifs forem verdadeiros
+    puts "Olá sou o else"
+end
+
+# Operador ternário
+# (condição) ? Condição verdadeira : condição falsa
+puts x == 5 ? "Condição verdadeira" : "Condição falsa"
