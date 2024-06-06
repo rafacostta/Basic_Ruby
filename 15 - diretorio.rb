5f13987d8517a930d72b05582aa02c739fe54e51
rafacostta <rafael.costta13@gmail.com>
2024-06-06 01:27:56 -0300

    Trabalahdno comdiretorios.rb

---
+++ 15 - diretorio.rb
@@ -0,0 +1,24 @@
+# DIRETORIO
+
+# Navegando nos diretorios
+# Classe Dir
+
+# Dir.chdir - Identifica o diretorio atual
+puts Dir.pwd()
+
+# Dir.chdir(diretorio_de_destino)
+Dir.chdir("/home/rafael/Área de Trabalho/ruby/Basic_Ruby")
+
+# Criando diretorios
+# Dir.mkdir("/Caminho/")
+
+# Listando diretorio
+# Dir.entries(Caminho_do_diretorio)
+puts Dir.pwd()
+puts Dir.entries(".")
+
+# Dir.foreach('caminho') - Lista os itens de um diretorio
+Dir.foreach(".") do |f|
+    puts f
+end
+
